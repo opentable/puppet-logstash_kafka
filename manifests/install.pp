4308f38bf3426641d88d92801a57e7b3af8e231d
liamjbennett <lbennett@opentable.com>
2014-09-30 12:36:28 +0100

    fixing bugs raised by strict checking

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,13 +3,8 @@
 # This private class is meant to be called from `logstash_kafka`.
 # It downloads and installs the logstash_kafka logstash plugin
 #
-class logstash_kafka::install(
-  $version = $logstash_kafka::version,
-  $install_dir = $logstash_kafka::install_dir,
-  $kafka_install_dir = $logstash_kafka::kafka_install_dir,
-  $logstash_install_dir = $logstash_kafka::logstash_install_dir,
-  $logstash_package_url = $logstash_kafka::logstash_package_url
-) {
+class logstash_kafka::install
+{
 
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
@@ -29,7 +24,7 @@ class logstash_kafka::install(
 
   if !defined(Class['logstash']) {
     class { 'logstash':
-      package_url  => $logstash_package_url,
+      package_url  => $logstash_kafka::logstash_package_url,
       java_install => false
     }
   }
@@ -37,54 +32,54 @@ class logstash_kafka::install(
   Class['java'] -> Class['kafka']
   Class['java'] -> Class['logstash']
 
-  file { "${install_dir}-${version}":
+  file { "${logstash_kafka::install_dir}-${logstash_kafka::version}":
     ensure => directory
   }
 
   exec { 'download-logstash-kafka':
-    command => "/usr/bin/wget -O /tmp/logstash-kafka-v${version}.tar.gz https://codeload.github.com/joekiller/logstash-kafka/tar.gz/v${version}",
-    unless  => "/usr/bin/test -f /tmp/logstash-kafka-v${version}.tar.gz"
+    command => "/usr/bin/wget -O /tmp/logstash-kafka-v${logstash_kafka::version}.tar.gz https://codeload.github.com/joekiller/logstash-kafka/tar.gz/v${logstash_kafka::version}",
+    unless  => "/usr/bin/test -f /tmp/logstash-kafka-v${logstash_kafka::version}.tar.gz"
   }
 
   exec { 'untar-logstash-kafka':
-    command => "/bin/tar -xvzf /tmp/logstash-kafka-v${version}.tar.gz -C ${install_dir}-${version} --strip-components=1",
-    unless  => "/usr/bin/test -d ${install_dir}-${version}/lib",
-    require => [ File["${install_dir}-${version}"], Exec['download-logstash-kafka'] ]
+    command => "/bin/tar -xvzf /tmp/logstash-kafka-v${logstash_kafka::version}.tar.gz -C ${logstash_kafka::install_dir}-${logstash_kafka::version} --strip-components=1",
+    unless  => "/usr/bin/test -d ${logstash_kafka::install_dir}-${logstash_kafka::version}/lib",
+    require => [ File["${logstash_kafka::install_dir}-${version}"], Exec['download-logstash-kafka'] ]
   }
 
   exec { 'install-jruby-kafka':
-    command     => "/usr/bin/java -jar vendor/jar/jruby-complete-1.7.11.jar --1.9 ${install_dir}/gembag.rb ${install_dir}/logstash-kafka.gemspec",
-    cwd         => $logstash_install_dir,
+    command     => "/usr/bin/java -jar vendor/jar/jruby-complete-1.7.11.jar --1.9 ${logstash_kafka::install_dir}/gembag.rb ${logstash_kafka::install_dir}/logstash-kafka.gemspec",
+    cwd         => $logstash_kafka::logstash_install_dir,
     environment => 'GEM_HOME=vendor/bundle/jruby/1.9/ GEM_PATH= ',
-    unless      => "/usr/bin/test -d ${logstash_install_dir}/vendor/bundle/jruby/1.9/gems/jruby-kafka-0.1.0"
+    unless      => "/usr/bin/test -d ${logstash_kafka::logstash_install_dir}/vendor/bundle/jruby/1.9/gems/jruby-kafka-0.1.0"
   }
 
   file { $install_dir:
     ensure => link,
-    target => "${install_dir}-${version}"
+    target => "${logstash_kafka::install_dir}-${logstash_kafka::version}"
   }
 
-  file { "${logstash_install_dir}/vendor/jar/kafka":
+  file { "${logstash_kafka::logstash_install_dir}/vendor/jar/kafka":
     ensure  => directory,
     require => Class['logstash']
   }
 
-  file { "${logstash_install_dir}/vendor/jar/kafka/libs":
+  file { "${logstash_kafka::logstash_install_dir}/vendor/jar/kafka/libs":
     ensure  => link,
-    target  => "${kafka_install_dir}/libs",
-    require => File["${logstash_install_dir}/vendor/jar/kafka"]
+    target  => "${logstash_kafka::kafka_install_dir}/libs",
+    require => File["${logstash_kafka::logstash_install_dir}/vendor/jar/kafka"]
   }
 
 #logstash::plugin
-  file { "${logstash_install_dir}/lib/logstash/inputs/kafka.rb":
+  file { "${logstash_kafka::logstash_install_dir}/lib/logstash/inputs/kafka.rb":
     ensure  => link,
-    target  => "${install_dir}/lib/logstash/inputs/kafka.rb",
+    target  => "${logstash_kafka::install_dir}/lib/logstash/inputs/kafka.rb",
     require => Class['logstash']
   }
 
-  file { "${logstash_install_dir}/lib/logstash/outputs/kafka.rb":
+  file { "${logstash_kafka::logstash_install_dir}/lib/logstash/outputs/kafka.rb":
     ensure  => link,
-    target  => "${install_dir}/lib/logstash/outputs/kafka.rb",
+    target  => "${logstash_kafka::install_dir}/lib/logstash/outputs/kafka.rb",
     require => Class['logstash']
   }
 }
